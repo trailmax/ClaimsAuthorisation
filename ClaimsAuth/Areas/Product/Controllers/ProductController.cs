f6233cdd3e490ffe1ec4948819849684a40b5b82
trailmax <trailmax1@gmail.com>
2014-07-25 17:19:41 +0100

    More dummy controllers

---
+++ ClaimsAuth/Areas/Product/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Mvc;
+
+
+namespace ClaimsAuth.Areas.Product.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(String id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+           
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(String id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
