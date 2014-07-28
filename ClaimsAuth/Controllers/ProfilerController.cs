d3d8157f13a1091fcf31620bd2d7f20dda5e3cbe
trailmax <trailmax1@gmail.com>
2014-07-28 01:34:24 +0100

    Claims Profiler

---
+++ ClaimsAuth/Controllers/ProfilerController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ClaimsAuth.Infrastructure.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+
+namespace ClaimsAuth.Controllers
+{
+    public class ProfilerController : Controller
+    {
+        private readonly UserManager userManager;
+
+
+        public ProfilerController(UserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddClaims()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> AddClaims(AddProfileClaimsViewModel viewModel)
+        {
+            var userId = ClaimsPrincipal.Current.Identity.GetUserId();
+            var user = await userManager.FindByIdAsync(userId);
+
+            for (int i = 0; i < viewModel.NumberOfClaims; i++)
+            {
+                var result =
+                    await userManager.AddClaimAsync(userId, new Claim(viewModel.ClaimType, viewModel.ClaimValue));
+            }
+
+            await userManager.SignInAsync(AuthenticationManager, user, true);
+
+
+            return RedirectToAction("Index");
+        }
+
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+
+    }
+
+
+    public class AddProfileClaimsViewModel
+    {
+        public String ClaimType { get; set; }
+        public String ClaimValue { get; set; }
+        public int NumberOfClaims { get; set; }
+    }
+}
